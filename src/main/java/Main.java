781ed3e5b36c81b9bf7ee4ab1b56da1ac5ac0fd9
nikolailjazovic <nikola.iljazovic@dice.hr>
2025-12-15 16:02:51 +0100

    Add index number to greeting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pozdrav iz aplikacije od Nikole Iljazovica");
+        System.out.println("Pozdrav iz aplikacije od Nikole Iljazovica (12345)");
     }
 }
